bc743014039361be463fc67e30bcf8db9d62aab9
NicolaiMartini <152643549+NicolaiMartini@users.noreply.github.com>
2025-12-04 16:08:43 +0100

    Slight changes for readability

--- server/monitoring/wazuh_install_script.sh
+++ server/monitoring/wazuh_install_script.sh
@@ -1,3 +1,5 @@
+# Remember to change permissions to executable, and run the file with sudo
+
 apt-get install gnupg apt-transport-https -y
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
 echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
