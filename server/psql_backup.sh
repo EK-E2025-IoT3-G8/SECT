613f97c3919d08f25746c25a51b4149d75038c7e
NicolaiMartini <152643549+NicolaiMartini@users.noreply.github.com>
2025-12-08 10:36:15 +0100

    psql_backup updated to reflect user permissions when using pg_dump

--- server/psql_backup.sh
+++ server/psql_backup.sh
@@ -4,6 +4,6 @@ DB_NAME="" # Update to db_name from script.py
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 FILE=$(BACKUP_DIR)/$(DB_NAME)_backup_$(TIME_STAMP).sql
 
-pg_dump "$DB_NAME" > "$FILE"
+pg_dump -U postgres "$DB_NAME" > "$FILE"
 
 scp "$FILE" device:/var/backup/sql/
