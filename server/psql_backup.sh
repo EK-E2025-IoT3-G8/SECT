978ff3ea7268491e00f7df3b79c02d702c509c36
NicolaiMartini <152643549+NicolaiMartini@users.noreply.github.com>
2025-12-08 14:40:57 +0100

    updated regarding backup of postgresql

--- server/psql_backup.sh
+++ server/psql_backup.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-BACKUP_DIR="/var/backups/psql"
-DB_NAME="" # Update to db_name from script.py
-TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-FILE=$(BACKUP_DIR)/$(DB_NAME)_backup_$(TIME_STAMP).sql
-
-pg_dump -U postgres "$DB_NAME" > "$FILE"
-
-scp "$FILE" device:/var/backup/sql/
+dump_file="$(date +%Y%m%d_%H%M%S).psql"
+pg_dump -U postgres -d hospital > "$dump_file" && scp "$dump_file" device:/home/sensor/ && rm "$dump_file"
