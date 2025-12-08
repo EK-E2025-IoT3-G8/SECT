8632959f7d1e6e58447b43bf146de93bc675aadc
NicolaiMartini <152643549+NicolaiMartini@users.noreply.github.com>
2025-12-08 10:12:25 +0100

    Cron, backup-script and ssh-config introduced

---
+++ server/psql_backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+BACKUP_DIR="/var/backups/psql"
+DB_NAME="" # Update to db_name from script.py
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+FILE=$(BACKUP_DIR)/$(DB_NAME)_backup_$(TIME_STAMP).sql
+
+pg_dump "$DB_NAME" > "$FILE"
+
+scp "$FILE" device:/var/backup/sql/
