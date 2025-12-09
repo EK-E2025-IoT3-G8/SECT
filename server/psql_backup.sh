fe00308babbf4cd6b04aafdf9e9d52e013604a9f
NicolaiMartini <152643549+NicolaiMartini@users.noreply.github.com>
2025-12-09 08:38:52 +0100

    Slight update to psql backup and cron

--- server/psql_backup.sh
+++ server/psql_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dump_file="$(date +%Y%m%d_%H%M%S).psql"
-pg_dump -U postgres -d hospital > "$dump_file" && scp "$dump_file" device:/home/sensor/ && rm "$dump_file"
+pg_dump -U postgres -d hospital > "$dump_file" && scp "$dump_file" device:/home/sensor/psql_backup && rm "$dump_file"
